f83c47d14849c2d7335e63f882a3bc6ccececece
Gabriel Takahashi <taakahashi@gmail.com>
2022-07-02 11:06:41 -0300

    Exemplificando o Princípio da Autovalidação

--- src/customer.ts
+++ src/customer.ts
@@ -10,13 +10,34 @@ class Customer {
         this._name = name;
         this._address = address;
         this._status = status;
+        
+        this.validate();
+    }
+
+    validate() {
+        if (this._id.length === 0) {
+            throw new Error("Id is required");
+        }
+
+        if (this._name.length === 0) {
+            throw new Error("Name is required");
+        }
+
+        if (this._address.length === 0) {
+            throw new Error("Address is required");
+        }
     }
 
     changeName(name: string) {
         this._name = name;
+        this.validate();
     }
 
     activate() {
+        if (this._address.length === 0) {
+            throw new Error("Address is mandatory to activate a customer");
+        }
+
         this._status = true;
     }
 
